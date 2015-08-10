a4f94f13ec9825cf4cbdeaa369ac4b739422be40
Nanda <nbprayuga@digdaya.com>
2015-08-10 20:36:20 +0700

    change

--- neo4jphp_connect_test.php
+++ neo4jphp_connect_test.php
@@ -1,5 +1,5 @@
 <?php
-    require __DIR__ .('vendor/autoload.php');
+    require __DIR__ .('/vendor/autoload.php');
     use Everyman\Neo4j\Client;
     $client = new Client('localhost', 7474);
     print_r($client->getServerInfo());
