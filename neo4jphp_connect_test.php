4dbde1635152044df5c27a1571f94b6d3e46951e
Nanda <nbprayuga@digdaya.com>
2015-08-10 20:01:48 +0700

    edit index method

--- neo4jphp_connect_test.php
+++ neo4jphp_connect_test.php
@@ -2,4 +2,4 @@
     require('vendor/autoload.php');
     use everyman\Neo4j\Client;
     $client = new Everyman\Neo4j\Client('localhost', 7474);
-    print_r($client->getServerInfo());`
+    print_r($client->getServerInfo());
