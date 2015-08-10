ef4d4717821d453e4b3a900ba0a21ec1429ca6d5
Nanda <nbprayuga@digdaya.com>
2015-08-10 20:18:57 +0700

    change

--- neo4jphp_connect_test.php
+++ neo4jphp_connect_test.php
@@ -1,5 +1,5 @@
 <?php
     require('vendor/autoload.php');
     use Everyman\Neo4j\Client;
-    $client = new Everyman\Neo4j\Client('localhost', 7474);
+    $client = new Client('localhost', 7474);
     print_r($client->getServerInfo());
